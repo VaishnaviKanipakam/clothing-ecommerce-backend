4a71e75cbdb5441f7c2aab26b27490747073d142
VaishnaviKanipakam <kanipakamvaishnavi1998@gmail.com>
2025-12-27 15:55:46 +0530

    dotenv package is installed  to load variables from a .env file into process.env

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config(); 
 const express = require("express");
 const app = express();
 const mysql = require("mysql2");
@@ -7,26 +8,43 @@ const bcrypt = require("bcrypt");
 const { request } = require("http");
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://collaborative-task-manager-frontend-pearl.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 let db = null;
 const initializeDbAndServer = async () => {
   try {
-    db = mysql.createConnection({
-      host: "localhost",
-      user: "vaishu",
-      password: "Bharu@96",
-      database: "clothing_ecommerce_backend",
-      insecureAuth: true,
+    db = mysql.createPool({
+      host:  process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: Number(process.env.DB_PORT || 3306),
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0,
     });
-    const port = 5000;
+    const port = process.env.PORT || 5000;
     app.listen(port, () => {
       console.log(`app listening at ${port}...`);
     });
-    db.connect(function (err) {
-      if (err) throw err;
-      console.log("Conected!");
-    });
+   db.getConnection((err, connection) => {
+  if (err) {
+    console.error("Database connection failed:", err.message);
+  } else {
+    console.log("Database connected successfully!");
+    connection.release();
+  }
+}); 
   } catch (e) {
     console.log(`DB Error: ${e.message}`);
     process.exit(1);
